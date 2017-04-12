bb63cf632f6038403b6b3e70bad5314979712b8c
DominicBrett <dombrett777@outlook.com>
2017-04-12 19:51:46 +0100

    Application Control

---
+++ DeviceManagmentSystem/src/control/ApplicationControl.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package control;
+
+
+import model.ApplicationModel;
+import view.ApplicationViewer;
+
+/**
+ *
+ * @author dominic
+ */
+public class ApplicationControl {
+     public static void main(String[] args){
+      ApplicationModel am = new ApplicationModel();
+      ApplicationModel.setInstance(am);
+      am.readDevices("src/devices.txt");
+   
+     
+   ApplicationViewer av = new ApplicationViewer();
+   ApplicationViewer.setInstance(av);
+        ApplicationViewer.getInstance().jFramePrint();
+        
+   
+      
+    }
+}
